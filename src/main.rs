5cd93e082154b4b070c247d4c44151af79392edb
t56k <txnash@gmail.com>
2023-03-06 15:04:11 +1100

    db

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
+use diesel::prelude::*;
 use std::{error::Error, io, process};
 
-mod shot;
+mod models;
+mod schema;
 
-fn parse() -> Result<(), Box<dyn Error>> {
+fn parse_csv() -> Result<Vec<models::NewShot>, Box<dyn Error>> {
     let mut csv_reader = csv::Reader::from_reader(io::stdin());
-    let mut shots = Vec::new();
+    let mut shots = vec![];
 
     for result in csv_reader.deserialize() {
-        let shot: shot::Shot = result?;
+        let shot: models::NewShot = result?;
         shots.push(shot);
     }
 
-    Ok(())
+    Ok(shots)
+}
+
+pub fn import_shots(conn: &mut PgConnection) -> Vec<models::Shot> {
+    let shots = parse_csv();
+
+    diesel::insert_into(shots::table)
+        .values(&shots)
+        .get_results(conn)
+        .expect("Error importing shots")
 }
 
 fn main() {
-    if let Err(err) = parse() {
-        println!("error parsing: {}", err);
-        process::exit(1);
-    }
+    let connection = &mut establish_connection();
+    let shots = import_shots(connection);
+
+    println!("Imported (first): {} with id {}", shots[0], shots[0].id);
 }
