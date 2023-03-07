6e20bcd48d84f3f454da117d986fec1c6727ce27
t56k <txnash@gmail.com>
2023-03-07 19:11:11 +1100

    allows for entering name

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,9 @@ use tacks::establish_connection;
 mod schema;
 mod shot;
 
-fn import_csv(connection: &mut PgConnection) -> Result<Vec<shot::NewShot>> {
-    use crate::schema::shots::dsl::shots;
+use schema::shots::dsl::*;
 
+fn import_csv(connection: &mut PgConnection) -> Result<Vec<shot::NewShot>> {
     let mut csv_reader = csv::Reader::from_reader(io::stdin());
     let mut new_shots = vec![];
 
@@ -27,8 +27,6 @@ fn import_csv(connection: &mut PgConnection) -> Result<Vec<shot::NewShot>> {
 }
 
 fn main() -> Result<()> {
-    use crate::schema::shots::dsl::shots;
-
     let connection = &mut establish_connection();
 
     let res = shots.limit(1).load::<shot::Shot>(connection)?;
@@ -36,8 +34,15 @@ fn main() -> Result<()> {
         let _ = import_csv(connection)?;
     }
 
-    let res = shots.limit(5).load::<shot::Shot>(connection)?;
-    println!("Displaying {} shots", res.len());
+    let mut buffer = String::new();
+    println!("Enter a shooter:");
+
+    io::stdin().read_line(&mut buffer)?;
+
+    let name: &str = buffer.trim();
+    let res = shots
+        .filter(shooter_name.eq(name))
+        .load::<shot::Shot>(connection)?;
 
     for s in res {
         println!("{:#?}", s);
