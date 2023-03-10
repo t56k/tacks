63c8207fc434aa3f516149b51ce41ed61c0f4416
t56k <txnash@gmail.com>
2023-03-10 13:01:25 +1100

    okay were good

--- src/main.rs
+++ src/main.rs
@@ -18,17 +18,19 @@ fn main() -> Result<()> {
         let mut input = String::new();
         io::stdin().read_line(&mut input)?;
 
-        let cmd = input.trim();
-        match cmd {
-            "games" => {
-                println!("importing games...");
-                import_games(conn, "data/all_teams.csv")?;
+        let cmd: Vec<&str> = input.trim().split_whitespace().collect();
+        match cmd.get(0).unwrap_or(&"") {
+            &"games" => {
+                let games_file = cmd.get(1).unwrap_or(&"data/all_teams.csv");
+                println!("importing games from {}...", games_file);
+                import_games(conn, games_file)?;
             }
-            "shots" => {
-                println!("importing shots...");
-                import_shots(conn, "data/shots_2022_1.csv")?;
+            &"shots" => {
+                let shots_file = cmd.get(1).unwrap_or(&"data/shots_2022_1.csv");
+                println!("importing shots from {}...", shots_file);
+                import_shots(conn, shots_file)?;
             }
-            "players" => {
+            &"players" => {
                 println!("fetching players...");
                 let players = shot::Shot::all_shooters(conn)?;
 
@@ -37,7 +39,9 @@ fn main() -> Result<()> {
                 }
             }
             _ => {
-                let shots = shot::Shot::by_shooter(conn, &cmd)?;
+                let name = cmd.join(" ");
+                let shots = shot::Shot::by_shooter(conn, &name)?;
+                println!("fetching {}...", name);
 
                 for shot in shots {
                     println!("Time: {}", shot.time);
@@ -46,7 +50,5 @@ fn main() -> Result<()> {
                 }
             }
         }
-
-        input.clear();
     }
 }
